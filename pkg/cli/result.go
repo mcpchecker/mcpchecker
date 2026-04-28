f7fcb1c9d0c69d729532efeef71b6ddbe2c46084
Jefferson Ramos <jeramos@redhat.com>
2026-04-28 11:15:33 -0300

    feat: Added the command `convert` to convert eval results to other formats
    Added the sub-command `convert` under `result` command and  nested `junit` converter command under this new command

--- pkg/cli/result.go
+++ pkg/cli/result.go
@@ -18,7 +18,7 @@ These commands operate on the JSON result files produced by 'mcpchecker check'.`
 	resultCmd.AddCommand(NewVerifyCmd())
 	resultCmd.AddCommand(NewSummaryCmd())
 	resultCmd.AddCommand(NewDiffCmd())
-	resultCmd.AddCommand(NewJUnitCmd())
+	resultCmd.AddCommand(NewConvertCmd())
 
 	return resultCmd
 }
